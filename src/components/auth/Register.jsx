5fc922a051852d8ea0d684aa4807a7bab02f4b08
Alok Kumar Naik <aloknaik016261@gmail.com>
2026-01-24 11:37:16 +0530

    feat: add Register page

---
+++ src/components/auth/Register.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import RegisterForm from '../../components/auth/RegisterForm';
+
+const Register = () => {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/');
+    }
+  }, [isAuthenticated, navigate]);
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-primary-50 to-primary-100 flex items-center justify-center p-4">
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default Register;
