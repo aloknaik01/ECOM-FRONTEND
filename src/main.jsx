1da29c93362d1e66d9b034ec300d6cbc27b2fcbe
aloknaik01 <aloknaik016261@gmail.com>
2026-01-29 22:13:09 +0530

    feat: setup app entry with Redux provider and theme initialization

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store/store.js';
-import App from './App.jsx'
+import App from './App.jsx';
+import './index.css';
 
 // Remove preload class after initial render to enable transitions
 window.addEventListener('load', () => {
@@ -9,7 +11,9 @@ window.addEventListener('load', () => {
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
+  </React.StrictMode>
 );
